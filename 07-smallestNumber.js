78b31c2e8b085dc3b25aa9f00baa102ebd97feb1
F1984898 Celso Rodrigues de Freitas Junior <celsojunior@bb.com.br>
2022-12-21 20:47:10 -0300

    Desafio 07 completado

--- 07-smallestNumber.js
+++ 07-smallestNumber.js
@@ -4,15 +4,22 @@
 const exampleArray = [-2, 0, 5, 9, 120, -60];
 
 function smallestNumber(array) {
-    console.log("complete com o código aqui! :)");
+    let smallNumber;
+    for (i = 0; i <= array.length; i++) {
+        if (i == 0) {
+            smallNumber = array[i];
+        } else if (array[i] < smallNumber) {
+            smallNumber = array[i];
+        }
+    }
+    return smallNumber;
 }
-
 
 
 
 // ________________________ TEST ________________________
 console.log('TESTS:')
-if(smallestNumber(exampleArray) == -60) {
+if (smallestNumber(exampleArray) == -60) {
     console.log("✅ passed");
 } else {
     console.log("❌ failed");
